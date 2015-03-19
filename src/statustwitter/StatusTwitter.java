95fa28b6240cc1b222af137d24067eb594d3c502
rfernandezgonzalez <rfernandezgonzalez@eniac04.danielcastelao.org>
2015-03-19 09:10:08 +0100

    2º commit, echo el merge y resuelto los conflictos en el Main!

--- src/statustwitter/StatusTwitter.java
+++ src/statustwitter/StatusTwitter.java
@@ -1,11 +1,13 @@
 package statustwitter;
 
+import twitter4j.Paging;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Clase para recobrar los twitts del morrazo
@@ -18,7 +20,18 @@ public class StatusTwitter {
      * @throws twitter4j.TwitterException
      */
     public static void main(String[] args) throws TwitterException {
-     
+
+        Twitter twitter;
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("cCwlXGbOea4tWifNEsWSYZowp")
+                .setOAuthConsumerSecret("nVckoYIfas2ucRVUROQcB0s6ZDHCpjOdvw8G6i0v72ilKY0xKy")
+                .setOAuthAccessToken("3055269988-3EClzoFaUspMzlM1O7tTebuGe3vyi5rCkSU3LiB")
+                .setOAuthAccessTokenSecret("ibohhy5rRk0ue0SQ3zhCUpmvhfEXCooULhtKfs2PUEwL5");
+        twitter = new TwitterFactory(cb.build()).getInstance();
+
+        Paging pagina = new Paging();
+
         Twitter mitwitter = new TwitterFactory().getInstance();
  
         Query query = new Query("#Cangas");
